f60f7930a53a91be93d399d023532a1e2166871e
Shaka <yhpjs@sina.com.cn>
2019-01-21 22:29:22 +0800

    First creat project Add Jog button

---
+++ laserMachineView.cpp
@@ -0,0 +1,144 @@
+// This MFC Samples source code demonstrates using MFC Microsoft Office Fluent User Interface
+// (the "Fluent UI") and is provided only as referential material to supplement the
+// Microsoft Foundation Classes Reference and related electronic documentation
+// included with the MFC C++ library software.
+// License terms to copy, use or distribute the Fluent UI are available separately.
+// To learn more about our Fluent UI licensing program, please visit
+// https://go.microsoft.com/fwlink/?LinkId=238214.
+//
+// Copyright (C) Microsoft Corporation
+// All rights reserved.
+
+// laserMachineView.cpp : implementation of the ClaserMachineView class
+//
+
+#include "stdafx.h"
+// SHARED_HANDLERS can be defined in an ATL project implementing preview, thumbnail
+// and search filter handlers and allows sharing of document code with that project.
+#ifndef SHARED_HANDLERS
+#include "laserMachine.h"
+#endif
+
+#include "laserMachineDoc.h"
+#include "laserMachineView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// ClaserMachineView
+
+IMPLEMENT_DYNCREATE(ClaserMachineView, CView)
+
+BEGIN_MESSAGE_MAP(ClaserMachineView, CView)
+	// Standard printing commands
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &ClaserMachineView::OnFilePrintPreview)
+	ON_WM_CONTEXTMENU()
+	ON_WM_RBUTTONUP()
+	ON_COMMAND(ID_BUTTON_XPLUS, &ClaserMachineView::OnButtonXplus)
+END_MESSAGE_MAP()
+
+// ClaserMachineView construction/destruction
+
+ClaserMachineView::ClaserMachineView() noexcept
+{
+	// TODO: add construction code here
+
+}
+
+ClaserMachineView::~ClaserMachineView()
+{
+}
+
+BOOL ClaserMachineView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: Modify the Window class or styles here by modifying
+	//  the CREATESTRUCT cs
+
+	return CView::PreCreateWindow(cs);
+}
+
+// ClaserMachineView drawing
+
+void ClaserMachineView::OnDraw(CDC* /*pDC*/)
+{
+	ClaserMachineDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: add draw code for native data here
+}
+
+
+// ClaserMachineView printing
+
+
+void ClaserMachineView::OnFilePrintPreview()
+{
+#ifndef SHARED_HANDLERS
+	AFXPrintPreview(this);
+#endif
+}
+
+BOOL ClaserMachineView::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// default preparation
+	return DoPreparePrinting(pInfo);
+}
+
+void ClaserMachineView::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: add extra initialization before printing
+}
+
+void ClaserMachineView::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: add cleanup after printing
+}
+
+void ClaserMachineView::OnRButtonUp(UINT /* nFlags */, CPoint point)
+{
+	ClientToScreen(&point);
+	OnContextMenu(this, point);
+}
+
+void ClaserMachineView::OnContextMenu(CWnd* /* pWnd */, CPoint point)
+{
+#ifndef SHARED_HANDLERS
+	theApp.GetContextMenuManager()->ShowPopupMenu(IDR_POPUP_EDIT, point.x, point.y, this, TRUE);
+#endif
+}
+
+
+// ClaserMachineView diagnostics
+
+#ifdef _DEBUG
+void ClaserMachineView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void ClaserMachineView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+ClaserMachineDoc* ClaserMachineView::GetDocument() const // non-debug version is inline
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(ClaserMachineDoc)));
+	return (ClaserMachineDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// ClaserMachineView message handlers
+
+
+void ClaserMachineView::OnButtonXplus()
+{
+	MessageBox(_T("X+"));
+}
---
+++ resource.h
@@ -0,0 +1,76 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by laserMachine.rc
+//
+#define IDD_ABOUTBOX                    100
+#define IDP_OLE_INIT_FAILED             100
+#define IDP_SOCKETS_INIT_FAILED         104
+#define IDB_WRITESMALL                  110
+#define IDB_WRITELARGE                  111
+#define IDB_MAIN                        112
+#define IDB_BUTTONS                     113
+#define IDB_FILELARGE                   114
+#define IDB_FILESMALL                   115
+#define IDR_POPUP_EDIT                  119
+#define ID_STATUSBAR_PANE1              120
+#define ID_STATUSBAR_PANE2              121
+#define IDS_STATUS_PANE1                122
+#define IDS_STATUS_PANE2                123
+#define IDR_MAINFRAME                   128
+#define IDR_MAINFRAME_256               129
+#define IDR_laserMachineTYPE            130
+#define ID_VIEW_OUTPUTWND               149
+#define IDR_PASTE_MENU                  151
+#define IDR_WINDOWS_MENU                152
+#define IDS_OUTPUT_WND                  157
+#define IDI_OUTPUT_WND                  165
+#define IDI_OUTPUT_WND_HC               166
+#define IDR_OUTPUT_POPUP                182
+#define ID_VIEW_NAVIGATION              185
+#define IDB_NAVIGATION_LARGE            186
+#define IDB_NAVIGATION_LARGE_HC         187
+#define IDB_PAGES                       188
+#define IDB_PAGES_HC                    189
+#define IDB_PAGES_SMALL                 190
+#define IDB_PAGES_SMALL_HC              191
+#define ID_TOOLS_OPTIONS                220
+#define ID_VIEW_CAPTION_BAR             221
+#define IDB_INFO                        230
+#define IDS_CAPTION_BUTTON              231
+#define IDS_CAPTION_BUTTON_TIP          232
+#define IDS_CAPTION_TEXT                233
+#define IDS_CAPTION_IMAGE_TIP           234
+#define IDS_CAPTION_IMAGE_TEXT          235
+#define IDS_MYCALENDARS                 250
+#define IDS_CALENDAR                    251
+#define IDS_SHORTCUTS                   252
+#define IDS_FOLDERS                     253
+#define IDS_OUTLOOKBAR                  258
+#define IDS_BUILD_TAB                   300
+#define IDS_DEBUG_TAB                   301
+#define IDS_FIND_TAB                    302
+#define IDS_EDIT_MENU                   306
+#define IDR_RIBBON                      307
+#define ID_WRITE_PASTEASHYPERLINK       32770
+#define ID_BUTTON2                      32772
+#define ID_BUTTON3                      32773
+#define ID_BUTTON_XPLUS                 32776
+#define ID_BUTTON6                      32777
+#define ID_BUTTON_YPLUS                 32778
+#define ID_BUTTON_ZPLUS                 32779
+#define ID_BUTTON_XMINUS                32780
+#define ID_BUTTON_YMINUS                32781
+#define ID_BUTTON_ZMINUS                32783
+#define ID_CHECK2                       32784
+#define ID_COMBO2                       32785
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        311
+#define _APS_NEXT_COMMAND_VALUE         32786
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           310
+#endif
+#endif
