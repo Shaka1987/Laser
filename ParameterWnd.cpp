15ab42f96fa90437d18173bbb77ddaaf05ba958f
shaka <yhpjs@sina.com.cn>
2021-07-18 00:06:59 +0800

    Add Property list in Parameter page

--- ParameterWnd.cpp
+++ ParameterWnd.cpp
@@ -23,6 +23,8 @@ CParameterWnd::~CParameterWnd()
 BEGIN_MESSAGE_MAP(CParameterWnd, CDockablePane)
 
 	ON_WM_ERASEBKGND()
+	ON_WM_CREATE()
+	ON_WM_SIZE()
 END_MESSAGE_MAP()
 
 
@@ -41,4 +43,67 @@ BOOL CParameterWnd::OnEraseBkgnd(CDC* pDC)
 	brushBackGround.CreateSolidBrush(RGB(0xff, 0xff, 0xff));
 	pDC->FillRect(rectClient, &brushBackGround);
 	return TRUE;
+}
+
+
+int CParameterWnd::OnCreate(LPCREATESTRUCT lpCreateStruct)
+{
+	if (CDockablePane::OnCreate(lpCreateStruct) == -1)
+		return -1;
+
+
+	CRect rectDummy;
+	rectDummy.SetRectEmpty();
+
+	if (!m_wndPropList.Create(WS_VISIBLE | WS_CHILD, rectDummy, this, 2))
+	{
+		TRACE0("Failed to create Properties Grid \n");
+		return -1;      // fail to create
+	}
+	InitPropList();
+	AdjustLayout();
+	return 0;
+}
+
+void CParameterWnd::AdjustLayout()
+{
+	if (GetSafeHwnd() == nullptr || (AfxGetMainWnd() != nullptr && AfxGetMainWnd()->IsIconic()))
+	{
+		return;
+	}
+
+	CRect rectClient;
+	GetClientRect(rectClient);
+	m_wndPropList.SetWindowPos(nullptr, rectClient.left, rectClient.top , rectClient.Width(), rectClient.Height() , SWP_NOACTIVATE | SWP_NOZORDER);
+
+}
+
+void CParameterWnd::InitPropList()
+{
+	m_wndPropList.EnableHeaderCtrl(FALSE);
+	m_wndPropList.EnableDescriptionArea();
+	m_wndPropList.SetVSDotNetLook();
+	m_wndPropList.MarkModifiedProperties();
+
+	CMFCPropertyGridProperty* pGroup1 = new CMFCPropertyGridProperty(_T("Appearance"));
+
+	pGroup1->AddSubItem(new CMFCPropertyGridProperty(_T("3D Look"), (_variant_t)false, _T("Specifies the window's font will be non-bold and controls will have a 3D border")));
+
+	CMFCPropertyGridProperty* pProp = new CMFCPropertyGridProperty(_T("Border"), _T("Dialog Frame"), _T("One of: None, Thin, Resizable, or Dialog Frame"));
+	pProp->AddOption(_T("None"));
+	pProp->AddOption(_T("Thin"));
+	pProp->AddOption(_T("Resizable"));
+	pProp->AddOption(_T("Dialog Frame"));
+	pProp->AllowEdit(FALSE);
+
+	pGroup1->AddSubItem(pProp);
+	pGroup1->AddSubItem(new CMFCPropertyGridProperty(_T("Caption"), (_variant_t)_T("About"), _T("Specifies the text that will be displayed in the window's title bar")));
+
+	m_wndPropList.AddProperty(pGroup1);
+}
+
+void CParameterWnd::OnSize(UINT nType, int cx, int cy)
+{
+	CDockablePane::OnSize(nType, cx, cy);
+	AdjustLayout();
 }
--- ParameterWnd.h
+++ ParameterWnd.h
@@ -6,14 +6,22 @@
 class CParameterWnd : public CDockablePane
 {
 	DECLARE_DYNAMIC(CParameterWnd)
+private:
 
+	CMFCPropertyGridCtrl m_wndPropList;
+
+	void InitPropList();
+	void AdjustLayout();
 public:
 	CParameterWnd();
 	virtual ~CParameterWnd();
+
 protected:
 	DECLARE_MESSAGE_MAP()
 public:
 	afx_msg BOOL OnEraseBkgnd(CDC* pDC);
+	afx_msg int OnCreate(LPCREATESTRUCT lpCreateStruct);
+	afx_msg void OnSize(UINT nType, int cx, int cy);
 };
 
 
